b6d8e740eede63bfa527e0f615dcb880bb75693f
Hans <hansolivas1@gmail.com>
2021-01-21 12:52:51 -0500

    new commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const SHARE = document.querySelector('.share')
+const SHARE_IMAGE = document.querySelector('.share__image')
+const SOCIALMEDIA = document.querySelector('.socialMedia')
+
+console.log(SHARE)
+
+function toggleSocialMedia() {
+	if(SOCIALMEDIA.classList[1] === 'isNoActive') {
+		SOCIALMEDIA.classList.remove('isNoActive')
+		SOCIALMEDIA.classList.add('isActive')
+		SHARE.setAttribute("class", "share share--darker")
+		SHARE_IMAGE.setAttribute("src", "./images/icon-share-white.svg")
+	}else {
+		SOCIALMEDIA.classList.remove('isActive')
+		SOCIALMEDIA.classList.add('isNoActive')
+		SHARE.setAttribute("class", "share")
+		SHARE_IMAGE.setAttribute("src", "./images/icon-share.svg")
+	}
+}
